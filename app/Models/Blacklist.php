3d4a0f43644eeade4c7c0eafd42c32d4068cd0c2
Dedan <dedanirungu@gmail.com>
2024-09-05 18:48:45 +0300

    Changes to files: app/Models/Blacklist.php app/Models/Whitelist.php

--- app/Models/Blacklist.php
+++ app/Models/Blacklist.php
@@ -3,6 +3,7 @@
 namespace Modules\Mail\Models;
 
 use Modules\Base\Models\BaseModel;
+use Modules\Mail\Models\Contact;
 
 class Blacklist extends BaseModel
 {
@@ -19,5 +20,14 @@ class Blacklist extends BaseModel
      * @var string
      */
     protected $table = "mail_blacklist";
+
+    /**
+     * Add relationship to Contact
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function contact()
+    {
+        return $this->belongsTo(Contact::class);
+    }
 
 }
