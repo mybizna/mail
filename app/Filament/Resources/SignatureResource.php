4eb2bfc005cf1d05b6a427f1f52df5b993194ec1
Dedan <dedanirungu@gmail.com>
2024-10-11 13:19:19 +0300

    Changes to files: app/Filament/Resources/BlacklistResource.php app/Filament/Resources/QueueResource.php app/Filament/Resources/SignatureResource.php app/Filament/Resources/ThemeResource.php app/Filament/Resources/WhitelistResource.php

--- app/Filament/Resources/SignatureResource.php
+++ app/Filament/Resources/SignatureResource.php
@@ -7,6 +7,7 @@ use Filament\Forms\Form;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Modules\Base\Filament\Resources\BaseResource;
+use Modules\Base\Filament\Resources\Pages;
 use Modules\Mail\Models\Signature;
 
 class SignatureResource extends BaseResource
@@ -78,4 +79,15 @@ class SignatureResource extends BaseResource
             ]);
     }
 
+    public static function getPages(): array
+    {
+
+        Pages\ListBase::setResource(static::class);
+
+        return [
+            'index' => Pages\ListBase::route('/'),
+            'create' => Pages\CreateBase::route('/create'),
+            'edit' => Pages\EditBase::route('/{record}/edit'),
+        ];
+    }
 }
